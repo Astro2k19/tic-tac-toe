85b59c318ef22a3af84df12ac13ffcc97bf2c10e
Astro2k19 <katartem200351@gmail.com>
2021-09-06 08:06:36 +0300

    feat: done game

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,60 @@
 class TicTacToe {
+
     constructor() {
-
-    }
-
+        this.currentPlayer = 'x'
+        this.gameField = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ]
+    }   
+  
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
-
+  
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.gameField[rowIndex][columnIndex]){
+            return;
+        }
+        this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+        this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
     }
-
+  
     isFinished() {
-
+        return this.getWinner() || this.isDraw() ? true : false;
     }
-
+  
     getWinner() {
-
+        for(let i = 0; i < 3; i++){
+            if(this.gameField[i][0] == this.gameField[i][1] && this.gameField[i][0] == this.gameField[i][2]){
+                return this.gameField[i][0];
+            }else if(this.gameField[0][i] == this.gameField[1][i] && this.gameField[0][i] == this.gameField[2][i]){
+                return this.gameField[0][i]
+            }
+  
+        }
+            if(this.gameField[0][0] == this.gameField[1][1] && this.gameField[0][0] == this.gameField[2][2]){
+                return this.gameField[0][0]
+            }else if(this.gameField[0][2] == this.gameField[1][1] && this.gameField[0][2] == this.gameField[2][0]){
+                return this.gameField[0][2]
+            }
+        
+            return null;
     }
-
+  
     noMoreTurns() {
-
+        return this.gameField.every(row => row.every(item => item));
     }
-
+  
     isDraw() {
-
+        return this.noMoreTurns() && !this.getWinner() 
     }
-
+  
     getFieldValue(rowIndex, colIndex) {
-
+        return this.gameField[rowIndex][colIndex] ? this.gameField[rowIndex][colIndex] : null;
     }
-}
+  }
+  
 
 module.exports = TicTacToe;
